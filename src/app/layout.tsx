1d4b0d2f981ef99ce1b07f187f59f1b2a1c40a0e
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-09-22 14:09:08 -0500

    add standard rules and more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavBar from '@/components/NavBar'
+// import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-pr w-12">
-      
-    </main>
+    <main className='flex min-h-screen flex-col items-center bg-pr w-12'></main>
   )
 }
