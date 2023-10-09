abb2d6d14045c700fe4e563d08c8ccda990bffdf
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-10-09 17:27:34 -0500

    improve NavBar

---
+++ src/app/components/LinkNav.tsx
@@ -0,0 +1,27 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+export default function LinkNav({
+  name,
+  path
+}: {
+  name: string
+  path: string
+}) {
+  const [, pathname] = usePathname().split('/')
+  console.log(pathname, path)
+  return (
+    <Link
+      href={`/${path}`}
+      className={cn(
+        'text-sm font-medium transition-colors hover:text-primary-text text-muted-foreground',
+        pathname === path && '!text-primary-text'
+      )}
+    >
+      {name}
+    </Link>
+  )
+}
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,25 +1,34 @@
-import Link from 'next/link'
 import AuthButton from './AuthButton'
+import GithubButton from './GithubButton'
+import LinkNav from './LinkNav'
 
+const Links = [
+  {
+    name: 'Dashboard',
+    path: 'dashboard'
+  },
+  {
+    name: 'Movements',
+    path: 'movements'
+  }
+]
 export default function NavBar() {
   return (
     <header className='text-white border-b-2 bg-primary border-border'>
       <nav className='flex items-center justify-between h-16 p-2 m-auto max-w-7xl'>
-        <ul className='flex gap-3'>
-          <li>
-            <Link href='/'>Overview</Link>
-          </li>
-          <li>
-            <Link href='/'>Customers</Link>
-          </li>
-          <li>
-            <Link href='/'>Products</Link>
-          </li>
-          <li>
-            <Link href='/'>Settings</Link>
-          </li>
-        </ul>
-        <AuthButton />
+        <div className='flex items-center gap-3'>
+          <p className='items-center hidden text-2xl font-bold md:flex'>
+            Cost-Manager
+          </p>
+          <GithubButton />
+        </div>
+        <div className='flex items-center space-x-4 lg:space-x-6'>
+          {Links.map(({ name, path }) => (
+            <LinkNav name={name} path={path} key={path} />
+          ))}
+
+          <AuthButton />
+        </div>
       </nav>
     </header>
   )
