40efbe92c800f427d6ceebc4d32cf54398f33a5a
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-10-12 20:13:32 -0500

    add some utils

---
+++ src/app/lib/numberToMonth.ts
@@ -0,0 +1,17 @@
+export function numberToMonth(number: number | string): string {
+  const months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ]
+  return months[Number(number)]
+}
