84427a211a327d79c71ba10a67b7ffbf0204b100
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-11-09 15:38:49 -0500

    Imported AUTH0_AUDIENCE and AUTH0_BASE_URL constants from const.ts in route.ts.

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -1,12 +1,13 @@
 // app/api/auth/[auth0]/route.js
+import { AUTH0_AUDIENCE, AUTH0_BASE_URL } from '@/constants/const'
 import { handleAuth, handleLogout, handleLogin } from '@auth0/nextjs-auth0'
 export const GET = handleAuth({
   login: handleLogin({
     authorizationParams: {
-      audience: 'https://cost-manager.com' // or AUTH0_AUDIENCE
+      audience: AUTH0_AUDIENCE // or AUTH0_AUDIENCE
       // Add the `offline_access` scope to also get a Refresh Token
       // scope: 'openid  profile email read:products' // or AUTH0_SCOPE
     }
   }),
-  logout: handleLogout({ returnTo: process.env.AUTH0_BASE_URL })
+  logout: handleLogout({ returnTo: AUTH0_BASE_URL })
 })
--- src/app/constants/const.ts
+++ src/app/constants/const.ts
@@ -1,4 +1,5 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000'
 
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? ''
+export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? ''
+export const AUTH0_BASE_URL = process.env.AUTH0_BASE_URL ?? ''
