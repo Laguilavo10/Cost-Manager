caf533383b6d56928a972ba9e55b7cadf7875193
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-12-01 16:01:52 -0500

    Rollback formatNumberAsCurrency

--- src/app/lib/formatNumberAsCurrency.ts
+++ src/app/lib/formatNumberAsCurrency.ts
@@ -1,13 +1,8 @@
 export function formatNumberAsCurrency(value: string | number) {
-  const numberValue = typeof value === 'string' ? parseInt(value) : value
-
-  if (numberValue >= 1000) {
-    return (numberValue / 1000).toFixed(0) + 'k'
-  } else {
-    return numberValue.toLocaleString('es-CO', {
-      style: 'currency',
-      currency: 'COP',
-      maximumFractionDigits: 0
-    })
-  }
+  const valueFormated = value.toLocaleString('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    maximumFractionDigits: 0
+  })
+  return valueFormated
 }
