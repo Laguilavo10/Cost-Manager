512c0b6cde8295fde19413714bd69b7831f75814
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2024-03-29 13:33:43 -0500

    Test max_age property in handleAuth function

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -6,7 +6,8 @@ export const GET = handleAuth({
     authorizationParams: {
       audience: AUTH0_AUDIENCE, // or AUTH0_AUDIENCE
       // Add the `offline_access` scope to also get a Refresh Token
-      scope: 'openid profile email offline_access' // or AUTH0_SCOPE
+      scope: 'openid profile email offline_access', // or AUTH0_SCOPE
+      max_age: 60 * 60 * 24 * 7 // 1 week|
     }
   }),
   logout: handleLogout({ returnTo: AUTH0_BASE_URL })
